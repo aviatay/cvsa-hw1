2802df10a60b1f1e9796bb90fe47e4e16d0d101f
aviatay <81303951+aviatay@users.noreply.github.com>
2025-06-12 16:51:57 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import cv2
 
 # Usage: python predict.py path/to/image.jpg
-image_path = image_path = sys.argv[1] if len(sys.argv) > 1 else 'path/to/default/image.jpg'            ## Run image from terminal or directly
+image_path = 'path/to/default/image.jpg'            ## Run image directly
 weights_path = 'weights/yolov8_best.pt' 
 
 model = YOLO(weights_path)
